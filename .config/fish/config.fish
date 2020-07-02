1eb50b43db20fbbfb2b6929ebbb140de16e64b42
euphmat <euphmat@gmail.com>
2020-07-02 13:51:21 +0900

    update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,14 @@
 set -g theme_display_date no
 set theme_color_scheme light
 
+# vim
+alias fishrc="vim ~/dotfiles/.config/fish/config.fish"
+
+# cd
+alias quiver="cd ~/Library/Containers/com.happenapps.Quiver/Data/Library/Application\ Support/Quiver"
+
+# Reload
+alias reload="source ~/dotfiles/.config/fish/config.fish"
+# rust commands
 alias ll="exa -hl"
 alias la="exa -hla"
