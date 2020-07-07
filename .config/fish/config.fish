6469e4928ae9f7126ff2533734b6a08ecbfeac55
euphmat <euphmat@gmail.com>
2020-07-07 13:16:55 +0900

    mod config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,4 @@
+# fish config
 set -g theme_display_date no
 set theme_color_scheme dracula
 
@@ -5,10 +6,11 @@ set theme_color_scheme dracula
 alias fishrc="vim ~/dotfiles/.config/fish/config.fish"
 
 # cd
-alias quiver="cd ~/Library/Containers/com.happenapps.Quiver/Data/Library/Application\ Support/Quiver"
+alias cdquiver="cd ~/Library/Containers/com.happenapps.Quiver/Data/Library/Application\ Support/Quiver"
 
 # Reload
 alias reload="source ~/dotfiles/.config/fish/config.fish"
+
 # rust commands
 alias ll="exa -hl"
 alias la="exa -hla"
