dcdecc803e9318e6b1cdaf70ad7a3e21959ab17e
unknown <jinzai9@technology.local>
2020-07-14 17:35:04 +0900

    mod brew.sh tapが必要なパッケージに対応

--- bin/brew.sh
+++ bin/brew.sh
@@ -4,6 +4,12 @@ set -Cu
 brewtxt=../doc/brew.txt
 # TODO waifu2xがインストールできない
 while read line
-        do
+do
+        if [[ $line == *waifu2x* ]]; then
+                echo "brew install imxieyi/waifu2x/$line"
+        elif [[ $line == *gphotos* ]]; then
+                echo "brew install gphotosuploader/tap/$line"
+        else
                 echo "brew install $line"
-        done < $brewtxt 
+        fi
+done < $brewtxt 
