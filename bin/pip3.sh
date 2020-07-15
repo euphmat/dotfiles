4cbef5b312d52130497da1ffb32430ab7b9be785
euphmat <euphmat@gmail.com>
2020-07-15 16:33:30 +0900

    pip3.sh

--- bin/pip3.sh
+++ bin/pip3.sh
@@ -6,4 +6,9 @@ pip3txt=../doc//pip3.txt
 while read line
 do
       pip3app=`echo $line | awk '{print $1} '`
+
+      if [[ $pip3app != *Package* ]] && [[ $pip3app != *-----------* ]]; then
+              echo "installing $pip3app..."
+              pip3 install $pip3app
+      fi
 done < $pip3txt
