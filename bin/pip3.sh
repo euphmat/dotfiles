696f486b24cdc612b54df9ed77c672f4820005cc
euphmat <euphmat@gmail.com>
2020-07-16 16:56:58 +0900

    update pip3.sh

--- bin/pip3.sh
+++ bin/pip3.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-set -C
+set -Cu
 
-pip3txt=../doc//pip3.txt
+data_source=../doc//pip3.txt
 
 while read line
 do
-      pip3app=`echo $line | awk '{print $1} '`
+      line_package_name=`echo $line | awk '{print $1} '`
 
-      if [[ $pip3app != *Package* ]] && [[ $pip3app != *-----------* ]]; then
-              echo "installing $pip3app..."
-              pip3 install $pip3app
+      if [[ $line_package_name != *Package* ]] && [[ $line_package_name != *-----------* ]]; then
+              if [[ `pip3 show $line_package_name 2>/dev/null; echo $?` != 1 ]]; then
+                      echo "✅ Already Installed $line_package_name"
+              else
+                      echo "⤵ Installing $line_package_name"
+                      pip3 install $line_package_name
+              fi 
       fi
-done < $pip3txt
+done < $data_source
