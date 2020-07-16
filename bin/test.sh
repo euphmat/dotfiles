e33a70a8defc138467424cbaca282e362af41038
euphmat <euphmat@gmail.com>
2020-07-16 17:07:02 +0900

    test.sh <- add pip.sh test

--- bin/test.sh
+++ bin/test.sh
@@ -36,3 +36,21 @@ do
                 echo "$line_package_name : ✖"
         fi
 done < $data_source
+
+
+data_source=../doc/pip3.txt
+echo "==========================================================="
+echo "  pip3.sh"
+echo "==========================================================="
+while read line
+do
+        line_package_name=`echo $line | awk '{print $1} '`
+        if [[ $line_package_name != *Package* ]] && [[ $line_package_name != *-----------* ]]; then
+
+                if [[ `pip3 show $line_package_name 2>/dev/null; echo $?` != 1 ]]; then
+                        echo "$line_package_name : ✅"
+                else
+                        echo "$line_package_name : ✖"
+                fi
+        fi
+done < $data_source
