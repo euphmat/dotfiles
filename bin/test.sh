fb991c8e1e044722b87284bae9929f94f764c26b
euphmat <euphmat@gmail.com>
2020-07-16 17:16:16 +0900

    update test.sh <- npm.txt

--- bin/test.sh
+++ bin/test.sh
@@ -12,13 +12,12 @@ echo "==========================================================="
 while read line
 do
         line_package_name=`echo $line | awk '{sub("@.*$", ""); print $2} '`
-        line_package_command=`echo $line | awk '{sub("@.*$", "");sub("-.*$", ""); print $2} '`
 
         if [[ $line_package_name != */usr/local/lib* ]] && [[ $line_package_name != "" ]]; then
-                if [[ `type $line_package_command 2>/dev/null; echo $?` != 1 ]]; then
-                        echo "$line_package_command : ✅"
+                if [[ `npm show $line_package_name 2>/dev/null; echo $?` != 1 ]]; then
+                        echo "$line_package_name : ✅"
                 else
-                        echo "$line_package_command : ✖"
+                        echo "$line_package_name : ✖"
                 fi
         fi
 done < $data_source
