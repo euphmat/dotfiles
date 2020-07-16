11f5867afb5fe64be3ff613dbdf502b4bf15e345
euphmat <euphmat@gmail.com>
2020-07-16 14:33:44 +0900

    update npm.sh

--- bin/npm.sh
+++ bin/npm.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-set -C
+set -Ceu
 
-npmtxt=../doc//npm.txt
+data_source=../doc//npm.txt
 
 while read line
 do
-        npmapp=`echo $line | awk '{sub("@.*$", ""); print $2} '`
+      line_package_name=`echo $line | awk '{sub("@.*$", ""); print $2} '`
+      line_package_command=`echo $line | awk '{sub("@.*$", "");sub("-.*$", ""); print $2} '`
 
-      if [[ $npmapp != */usr/local/lib* ]] && [[ $npmapp != "" ]]; then
-              echo "installing $npmapp"
-              npm install -g $npmapp
+      if [[ $line_package_name != */usr/local/lib* ]] && [[ $line_package_name != "" ]]; then
+              if [[ `type $line_package_command; echo $?` != 1 ]]; then
+                      echo "Already Installed $line_package_name"
+              else
+                      echo "installing $line_package_name"
+                      npm install $line_package_name
+              fi
       fi
-done < $npmtxt
+done < $data_source
