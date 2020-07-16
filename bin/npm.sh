aa4caaaa3fdea230a09243177475b9afdb4fd10f
euphmat <euphmat@gmail.com>
2020-07-16 17:12:52 +0900

    update npm.sh

--- bin/npm.sh
+++ bin/npm.sh
@@ -6,10 +6,9 @@ data_source=../doc//npm.txt
 while read line
 do
       line_package_name=`echo $line | awk '{sub("@.*$", ""); print $2} '`
-      line_package_command=`echo $line | awk '{sub("@.*$", "");sub("-.*$", ""); print $2} '`
 
       if [[ $line_package_name != */usr/local/lib* ]] && [[ $line_package_name != "" ]]; then
-              if [[ `type $line_package_command 2>/dev/null; echo $?` != 1 ]]; then
+              if [[ `npm info $line_package_name 2>/dev/null; echo $?` != 1 ]]; then
                       echo "✅ Already Installed $line_package_name"
               else
                       echo "⤵ installing $line_package_name"
