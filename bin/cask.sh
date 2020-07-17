449fff1871d8745ad4a0c670de16f3962b7c38f3
euphmat <euphmat@gmail.com>
2020-07-17 14:03:26 +0900

    refactoring

--- bin/cask.sh
+++ bin/cask.sh
@@ -18,5 +18,3 @@ done < $data_source
 ## iTerm2
 echo "Installing imgcat on iTerm2..."
 curl https://iterm2.com/utilities/imgcat > /usr/local/bin/imgcat && chmod +x /usr/local/bin/imgcat;
-
-echo "Finished"
