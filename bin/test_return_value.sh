b31ad6eedfe7af230ca7345c5c4c61d4e7ee28f9
euphmat <euphmat@gmail.com>
2020-07-18 20:37:47 +0900

    mod ok?

--- bin/test_return_value.sh
+++ bin/test_return_value.sh
@@ -9,7 +9,7 @@ echo -n "pip3 show : "; echo `pip3 show instaloader >/dev/null 2>&1; echo $?`
 echo -n "npm  fund : "; echo `npm fund ghwd >/dev/null 2>&1; echo $?`
 echo -n "brew list : "; echo `brew list exa >/dev/null 2>&1; echo $?`
 echo -n "cask list : "; echo `brew cask list Alfred >/dev/null 2>&1; echo $?`
-echo -n "mas  search : "; echo `mas search 539883307 >/dev/null 2>&1; echo $?`
+echo -n "mas  search : "; echo `grep "539883307" -rl ../doc/mas.txt >/dev/null 2>&1; echo $?`
 
 echo "======================================================"
 echo " テスト：未インストール && 存在するパッケージ"
@@ -19,7 +19,7 @@ echo -n "pip3 show : "; echo `pip3 show jupyterlab >/dev/null 2>&1; echo $?`
 echo -n "npm  fund : "; echo `npm -g fund gulp >/dev/null 2>&1; echo $?`
 echo -n "brew list : "; echo `brew list tokei >/dev/null 2>&1; echo $?`
 echo -n "cask list : "; echo `brew cask list discord >/dev/null 2>&1; echo $?`
-echo -n "mas  search : "; echo `mas search 747824928 >/dev/null 2>&1; echo $?`
+echo -n "mas  search : "; echo `grep "747824928" -rl ../doc/mas.txt >/dev/null 2>&1; echo $?`
 
 echo "======================================================"
 echo " テスト：存在しないパッケージ"
@@ -29,4 +29,4 @@ echo -n "pip3 show : "; echo `pip3 show jkjkjkjkjk >/dev/null 2>&1 ; echo $?`
 echo -n "npm  fund : "; echo `npm fund fdsfsdfa >/dev/null 2>&1; echo $?`
 echo -n "brew list : "; echo `brew list xfsdafea >/dev/null 2>&1; echo $?`
 echo -n "cask list : "; echo `brew cask list Aweflfed >/dev/null 2>&1; echo $?`
-echo -n "mas search  : "; echo `mas  search 53954543534534 >/dev/null 2>&1; echo $?`
+echo -n "mas  search : "; echo `grep "842378487387487" -rl ../doc/mas.txt >/dev/null 2>&1; echo $?`
