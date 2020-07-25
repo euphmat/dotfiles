4d12263161f1effbb24489aeea7e01926f47a707
euphmat <euphmat@gmail.com>
2020-07-25 17:01:56 +0900

    add updatetxt.sh

---
+++ bin/update_doc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -eu
+
+# Update Package
+brew upgrade
+brew cask upgrade
+pip3 list -o | tail -n +3 | awk '{ print $1 }' | xargs pip3 install -U
+npm install -g npm
+ncu -gu && npm update
+mas upgrade
+
+# Update txt
+mas list > ../doc/mas.txt
+brew list > ../doc/brew.txt
+brew cask list > ../doc/cask.txt
+npm ls --depth=0 -g > ../doc/npm.txt
+pip3 list > ../doc/pip3.txt
