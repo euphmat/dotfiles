823ff2ab1d90df66f938b53e409dfe59bd95f271
euphmat <euphmat@gmail.com>
2020-07-27 09:20:23 +0900

    mas addvariable package_appname

--- bin/mas.sh
+++ bin/mas.sh
@@ -6,10 +6,11 @@ data_source=../doc//mas.txt
 while read line
 do
       line_package_name=`echo $line | awk '{print $1} '`
+      line_package_appname=`echo $line | awk '{print $2} '`
       if [[ `grep $line_package_name -rl ../doc/mas.txt >/dev/null 2>&1; echo $?` = 0 ]] && [[ `mas search $line_package_name >/dev/null 2>&1; echo $?` = 0 ]]; then
-                echo "✅ Already Installed $line_package_name"
+                echo "✅ Already Installed $line_package_appname"
       else
-                echo "⤵ installing $line_package_name"
+                echo "⤵ installing $line_package_appname"
                 mas install $line_package_name
       fi
 done < $data_source
--- bin/test_installed_package.sh
+++ bin/test_installed_package.sh
@@ -37,10 +37,11 @@ data_source=../doc/mas.txt
 while read line
 do
         line_package_name=`echo $line | awk '{print $1} '`
+        line_package_appname=`echo $line | awk '{print $2} '`
         if [[ `grep $line_package_name -rl ../doc/mas.txt >/dev/null 2>&1; echo $?` = 0 ]] && [[ `mas search $line_package_name >/dev/null 2>&1; echo $?` = 0 ]]; then
-                echo "$line_package_name : ✅"
+                echo "$line_package_appname : ✅"
         else
-                echo "$line_package_name : ✖"
+                echo "$line_package_appname : ✖"
         fi
 done < $data_source
 
