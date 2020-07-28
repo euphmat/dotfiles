a707349e264dcd8e06b7e1e632f2280698999e97
euphmat <euphmat@gmail.com>
2020-07-28 16:34:55 +0900

    WIP

---
+++ bin/test_package_manager.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+resut=0
+echo -n "brew"
+if [[ `type brew > /dev/null 2>&1; echo $?` = 0 ]]; then
+        echo -e " : \033[0;32mSuccessful\033[0;39m"
+else
+        result=$((result + 1))
+        echo -e " : \033[0;31mFailed\033[0;39m"
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+echo -n "cask"
+if [[ `type 'brew cask' > /dev/null 2>&1; echo $?` = 0 ]]; then
+        echo -e " : \033[0;32mSuccessful\033[0;39m"
+else
+        result=$((result + 1))
+        echo -e " : \033[0;31mFailed\033[0;39m"
+fi
+
+echo -n "mas "
+if [[ `type mas > /dev/null 2>&1; echo $?` = 0 ]]; then
+        echo -e " : \033[0;32mSuccessful\033[0;39m"
+else
+        result=$((result + 1))
+        echo -e " : \033[0;31mFailed\033[0;39m"
+fi
+
+echo -n "pip "
+if [[ `type pip > /dev/null 2>&1; echo $?` = 0 ]]; then
+        echo -e " : \033[0;32mSuccessful\033[0;39m"
+else
+        result=$((result + 1))
+        echo -e " : \033[0;31mFailed\033[0;39m"
+fi
+
+echo -n "npm "
+if [[ `type npm > /dev/null 2>&1; echo $?` = 0 ]]; then
+        echo -e " : \033[0;32mSuccessful\033[0;39m"
+else
+        result=$((result + 1))
+        echo -e " : \033[0;31mFailed\033[0;39m"
+fi
+exit $result
