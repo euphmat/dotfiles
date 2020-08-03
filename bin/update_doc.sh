6ca2fb454e8f19701196d50bc557686124a4a6a2
euphmat <euphmat@gmail.com>
2020-08-03 12:30:23 +0900

    update_doc.sh

--- bin/update_doc.sh
+++ bin/update_doc.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 set -eu
 
+# =====================================================================
 # Update Package
+# =====================================================================
 brew upgrade
-brew cask upgrade
 pip3 list -o | tail -n +3 | awk '{ print $1 }' | xargs pip3 install -U
 npm install -g npm
 ncu -gu && npm update
 mas upgrade
 
-# Update txt
+# =====================================================================
+# Update Document
+# =====================================================================
 mas list > ../doc/mas.txt
 brew list > ../doc/brew.txt
 brew cask list > ../doc/cask.txt
