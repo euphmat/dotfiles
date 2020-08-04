8b0007d37b9e37a556e7c75d46eb1e61405283a3
euphmat <euphmat@gmail.com>
2020-08-04 09:27:37 +0900

    add iterm2 on imgcat

--- bin/setting_app.sh
+++ bin/setting_app.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -Cu
 
-## iTerm2
-echo "Installing imgcat on iTerm2..."
-curl https://iterm2.com/utilities/imgcat > /usr/local/bin/imgcat && chmod +x /usr/local/bin/imgcat;
+echo "=========================================================="
+echo " iTerm2"
+echo "=========================================================="
+if [[ `type imgcat > /dev/null 2>&1; echo $?` = 0 ]]; then
+        echo "Already Installed imgcat on iTerm2"
+else
+        echo "Installing imgcat on iTerm2..."
+        curl https://iterm2.com/utilities/imgcat > /usr/local/bin/imgcat && chmod +x /usr/local/bin/imgcat;
+fi
+
