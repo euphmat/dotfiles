b2518136cbd1edd98179a97c9aa016a1ce4b530b
euphmat <euphmat@gmail.com>
2020-11-27 10:18:56 +0900

    delete check_os

--- init.sh
+++ init.sh
@@ -1,34 +1,15 @@
 #!/usr/bin/env bash
-set -Ceuo pipefail
 
 function main(){
-        check_os
         move_src_directory
 
-        case $OS in
-                Mac ) 
-                        ./bin/01.setup_git.sh
-                        ./bin/02.setup_homebrew.sh 
-                        ./bin/03.install_package.sh
-                        ./bin/04.setup_fish.sh
-                        ./bin/deploy.sh
-                        ;;
-                Windows ) echo "$OS is not Supported." ;;
-                Linux ) echo "$OS is not Supported." ;;
-                Unknown ) echo "$OS is not Supported." ;;
-        esac
-}
+        ./bin/01.setup_git.sh
+        ./bin/02.setup_homebrew.sh 
+        ./bin/03.install_package.sh
+        ./bin/04.setup_fish.sh
 
-function check_os() {
-        if [[ "$(expr substr $(uname -s) 1 6)" == "Darwin" ]]; then
-                OS=Mac
-        elif [[ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]]; then
-                OS=Windows
-        elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
-                OS=Linux
-        else
-                OS=Unknown
-        fi
+        rm -rf ~.config/
+        ./bin/deploy.sh
 }
 
 function move_src_directory() {
