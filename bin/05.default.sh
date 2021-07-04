a4e06f4a96fc67c591197843499c019d0ec37fa7
euphmat <euphmat@gmail.com>
2021-07-05 08:53:24 +0900

    update default

--- bin/05.default.sh
+++ bin/05.default.sh
@@ -5,6 +5,7 @@ function main(){
         sudo pmset -a standbydelay 86400
         sudo nvram SystemAudioVolume=" "
         defaults write com.apple.dock persistent-apps -array
+        defaults write com.apple.finder AppleShowAllFiles TRUE
 }
 
 main
